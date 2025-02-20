b3f4d64beb7701148e07ad0c20fbb25ce98433e6
shak <iserve4u@gmail.com>
2025-02-20 08:51:04 -0500

    good locked pi at lvl

--- runner.sh
+++ runner.sh
@@ -1,3 +1,6 @@
+echo "Did you change the names of the Vars Files?"
+read blank
+
 ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i inventory/hosts.ini playbook.yml \
     --limit 'all' --skip-tags "none" --tags "containers"
 
